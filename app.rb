8afe45961baba0922e3657434132794173629035
Nick Inman <nick@theoatmeal.com>
2019-01-25 00:53:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 	 end
 
 	 post "/piglatinize" do
-	   @piglatin = PigLatinizer.new.piglatin(params[:user_phrase])
+	   @piglatin = PigLatinizer.new.piglatinize(params[:user_phrase])
 	   erb :results
 	 end
 
